e309f4020d9b14ad14296e9b9c52f51fa25a50a8
laugh1997 <48004715+laugh1997@users.noreply.github.com>
2019-04-03 19:13:04 +0800

    Add files via upload

---
+++ TypeNum.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class TypeNum
+{
+	public static void main(String[] args)
+	{
+	//将int类型48转为char并输出
+
+	//int num = 48;
+	//char char1 = (char)48;
+	//System.out.print(char1);
+
+	//将字符'中'转为int类型
+	 //int a = '中';
+	 //System.out.print(a);
+	 //方法
+	 //int x = 5;
+	 //int y = f(x);
+	 //System.out.print(y);
+	//}
+	//public static int f(int abc){
+	 //int result = abc*abc-2*abc+4;
+	 //return result;
+	 //运算符
+	 //int x = 9;
+	 //int y = f(x);
+	 //System.out.print(y);
+	//}
+	//public static int f(int abc){
+		//int result = abc%3;
+		//return result;
+		//第二个
+		//int x = 5;
+		//int y = f(x);
+		//System.out.print(y);
+	//}
+	//public static int f(int abc){
+		//int result = abc++;
+		//return result;
+		//这里是if 循环
+		//Scanner scanner = new Scanner(System.in);
+		//System.out.println("请输入数字");
+		//int score = scanner.nextInt();
+		//if(score>100||score<0){
+		//System.out.print("super man");
+		//}
+		//if(score>=90&&score<=100){
+		//System.out.print("优秀");
+		//}
+		//if(score>=80&&score<=90){
+			//System.out.print("良好");
+		//}
+		//if(score<80){
+			//System.out.print("不及格");
+		//}
+		//这里是while循环
+		Scanner scanner = new Scanner(System.in);
+		while(1==1){
+			System.out.println("请输入数字：");
+			int num = scanner.nextInt();//键盘输入语句
+			if(num==-1){
+				break;
+			}
+			System.out.print(num);
+		}
+		 
+		//Scanner scanner = new Scanner( System.in);
+		//System.out.print("请输入工资：");
+		//int salary = scanner.nextInt();
+		//if(salary<=5000)
+		//{
+			//System.out.print(salary-(salary*0.11));
+		//}
+		//else if(salary>5000&&salary<=8000);
+			//{
+			//System.out.print(salary-((salary*0.11)+(salary*0.03)));
+			//}
+				 //if(salary>8000&salary<=17000);
+				//{
+					//System.out.print(salary-(salary*0.11)+(salary*0.1));
+				//}
+	}
+}
